de94d4a92f27f59a4acd6b6ab13a15a5e948a540
Abu Sayem Amir <76561416+sayem-hub@users.noreply.github.com>
2022-05-21 14:51:38 +0600

    creating team-portfolio website

---
+++ section/topbar.php
@@ -0,0 +1,15 @@
+<!-- ======= Top Bar ======= -->
+  <section id="topbar" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-center justify-content-md-between">
+      <div class="contact-info d-flex align-items-center">
+        <i class="bi bi-envelope-fill"></i><a href="mailto:info@logic-run.com">info@aheadit.com</a>
+        <i class="bi bi-phone-fill phone-icon"></i> +880 1770716151
+      </div>
+      <div class="social-links d-none d-md-block">
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+      </div>
+    </div>
+  </section>
